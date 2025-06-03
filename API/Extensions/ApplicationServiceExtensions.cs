54adb6e3cb8655e0eb015ecb765b259df3a954f3
Alex <alex275221@gmail.com>
2025-06-03 11:22:56 -0300

    feat: Implementar extensiones de servicios de Identity y autenticación

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+
+        });
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        
+        return services;
+
+    }
+
+}
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,46 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddSignInManager<SignInManager<AppUser>>()            
+                .AddEntityFrameworkStores<DataContext>();
+
+            var tokenKey = config["TokenKey"];
+            if (string.IsNullOrEmpty(tokenKey))
+            {
+                throw new InvalidOperationException("La TokenKey no ha sido configurada.");
+            }
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+            
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+}
