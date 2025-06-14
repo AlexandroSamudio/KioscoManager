94c42e5786da48f78bd89ec8a3f8aee499f0929d
Alex <alex275221@gmail.com>
2025-06-14 19:08:20 -0300

    feat: mejorar manejo de errores y suscripción en el componente de dashboard y servicio de productos

--- client/src/app/_components/dashboard/dashboard.component.ts
+++ client/src/app/_components/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, WritableSignal, inject, signal } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  inject,
+  signal,
+  DestroyRef,
+} from '@angular/core';
 import { ProductoService } from '../../_services/producto.service';
 import { Producto } from '../../_models/producto.model';
 import { Venta } from '../../_models/venta.model';
@@ -6,18 +13,20 @@ import { ProductoMasVendido } from '../../_models/producto-mas-vendido.model';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { VentaService } from '../../_services/venta.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [NavbarComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   productoService = inject(ProductoService);
   ventaService = inject(VentaService);
+  destroyRef = inject(DestroyRef);
   lowestStockProducts: WritableSignal<Producto[]> = signal([]);
   totalVentasDia: WritableSignal<number> = signal(0);
   ventasRecientes: WritableSignal<Venta[]> = signal([]);
@@ -32,63 +41,79 @@ export class DashboardComponent implements OnInit {
       lowStock: this.productoService.getProductosByLowestStock(),
       totalVentas: this.ventaService.getTotalVentasDia(),
       recientes: this.ventaService.getVentasRecientes(),
-      masVendidos: this.productoService.getProductosMasVendidosDelDia()
-    }).subscribe({
-      next: ({ lowStock, totalVentas, recientes, masVendidos }) => {
-        this.lowestStockProducts.set(lowStock as Producto[]);
-        this.totalVentasDia.set(totalVentas as number);
-        this.ventasRecientes.set(recientes as Venta[]);
-        this.productosMasVendidosDelDia.set(masVendidos as ProductoMasVendido[]);
-      },
-      error: err => console.error('Error cargando dashboard', err)
-    });
+      masVendidos: this.productoService.getProductosMasVendidosDelDia(),
+    })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: ({ lowStock, totalVentas, recientes, masVendidos }) => {
+          this.lowestStockProducts.set(lowStock);
+          this.totalVentasDia.set(totalVentas);
+          this.ventasRecientes.set(recientes);
+          this.productosMasVendidosDelDia.set(masVendidos);
+        },
+        error: (err) => console.error('Error cargando dashboard', err),
+      });
   }
 
-  getTotalVentasDia():void{
-    this.ventaService.getTotalVentasDia().subscribe({
-      next: (data) => {
-        console.log('Total ventas del día:', data);
-        this.totalVentasDia.set(data as number);
-      },
-      error: (error) => {
-        console.error('Error fetching total ventas del día:', error);
-      }
-    });
+  getTotalVentasDia(): void {
+    this.ventaService
+      .getTotalVentasDia()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (data) => {
+          console.log('Total ventas del día:', data);
+          this.totalVentasDia.set(data as number);
+        },
+        error: (error) => {
+          console.error('Error fetching total ventas del día:', error);
+        },
+      });
   }
 
   getProductosByLowestStock(): void {
-    this.productoService.getProductosByLowestStock().subscribe({
-      next: (data) => {
-        console.log('Productos con menor stock:', data);
-        this.lowestStockProducts.set(data as Producto[]);
-      },
-      error: (error) => {
-        console.error('Error fetching productos by lowest stock:', error);
-      }
-    });
+    this.productoService
+      .getProductosByLowestStock()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (data) => {
+          console.log('Productos con menor stock:', data);
+          this.lowestStockProducts.set(data as Producto[]);
+        },
+        error: (error) => {
+          console.error('Error fetching productos by lowest stock:', error);
+        },
+      });
   }
-
   getVentasRecientes(): void {
-    this.ventaService.getVentasRecientes().subscribe({
-      next: (data) => {
-        console.log('Ventas recientes:', data);
-        this.ventasRecientes.set(data as Venta[]);
-      },
-      error: (error) => {
-        console.error('Error fetching ventas recientes:', error);
-      }
-    });
+    this.ventaService
+      .getVentasRecientes()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (data) => {
+          console.log('Ventas recientes:', data);
+          this.ventasRecientes.set(data as Venta[]);
+        },
+        error: (error) => {
+          console.error('Error fetching ventas recientes:', error);
+        },
+      });
   }
 
   getProductosMasVendidosDelDia(): void {
-    this.productoService.getProductosMasVendidosDelDia().subscribe({
-      next: (data) => {
-        console.log('Productos más vendidos del día:', data);
-        this.productosMasVendidosDelDia.set(data as ProductoMasVendido[]);
-      },
-      error: (error) => {
-        console.error('Error fetching productos más vendidos del día:', error);
-      }
-    });
+    this.productoService
+      .getProductosMasVendidosDelDia()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (data) => {
+          console.log('Productos más vendidos del día:', data);
+          this.productosMasVendidosDelDia.set(data as ProductoMasVendido[]);
+        },
+        error: (error) => {
+          console.error(
+            'Error fetching productos más vendidos del día:',
+            error
+          );
+        },
+      });
   }
 }
--- client/src/app/_services/producto.service.ts
+++ client/src/app/_services/producto.service.ts
@@ -1,30 +1,63 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
+import { Producto } from '../_models/producto.model';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ProductoMasVendido } from '../_models/producto-mas-vendido.model';
+import { NotificationService } from './notification.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductoService {
-
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
+  private notificationService = inject(NotificationService);
 
-  getProductos() {
-    return this.http.get(`${this.baseUrl}productos`);
+  getProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.baseUrl}productos`).pipe(
+      catchError((error) => {
+        this.notificationService.error('Error al obtener los productos', error);
+        return throwError(() => error);
+      })
+    );
   }
 
-  getProducto(id: number) {
-    return this.http.get(`${this.baseUrl}productos/${id}`);
+  getProducto(id: number): Observable<Producto> {
+    return this.http.get<Producto>(`${this.baseUrl}productos/${id}`).pipe(
+      catchError((error) => {
+        this.notificationService.error(
+          `Error al obtener el producto con ID ${id}`,
+          error
+        );
+        return throwError(() => error);
+      })
+    );
   }
 
-  getProductosByLowestStock() {
-    return this.http.get(`${this.baseUrl}productos/low-stock`);
+  getProductosByLowestStock(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.baseUrl}productos/low-stock`).pipe(
+      catchError((error) => {
+        this.notificationService.error(
+          'Error al obtener los productos con bajo stock',
+          error
+        );
+        return throwError(() => error);
+      })
+    );
   }
 
-  getProductosMasVendidosDelDia() {
-    return this.http.get(`${this.baseUrl}ventas/productos-mas-vendidos`);
+  getProductosMasVendidosDelDia(): Observable<ProductoMasVendido[]> {
+    return this.http
+      .get<ProductoMasVendido[]>(`${this.baseUrl}ventas/productos-mas-vendidos`)
+      .pipe(
+        catchError((error) => {
+          this.notificationService.error(
+            'Error al obtener los productos más vendidos del día',
+            error
+          );
+          return throwError(() => error);
+        })
+      );
   }
-
-
 }
